8f60f5bba8bcd7aa12e93222b04ef9f77cdb0440
34431 <344319484@qq.com>
2019-07-29 10:44:38 +0800

    new project

--- 认识python/01-HelloPython.py
+++
@@ -1,3 +0,0 @@
-print("Hello python")
-print("你好 世界")
-print("Hello Hello")
--- 马哥-python/test_01.py
+++
@@ -1,4 +0,0 @@
-meg = dict(map(lambda x:(x%5,x),range(500))) # 字典
-meg1 = list(map(lambda x:2*x+1,range(5))) #求奇数
-print(meg)
-print(meg1)
