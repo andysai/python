378c0a5723b931bfe7d7a4bb8aa114ac07cd034a
34431 <344319484@qq.com>
2019-10-15 16:18:01 +0800

    study

---
+++ 黑马程序员-python/01_Python基础/hm_30_第一个函数.py
@@ -0,0 +1,8 @@
+# 注意：定义好函数之后，只表示这个函数封装了一段代码而已
+def say_hello():
+
+    print("Hello 1")
+    print("Hello 2")
+    print("Hello 3")
+
+say_hello()
---
+++ 黑马程序员-python/01_Python基础/hm_31_第一个函数改造.py
@@ -0,0 +1,15 @@
+name = '小明'
+
+# Python解释器知道下方定义了一个函数
+def say_hello():
+
+    print("Hello 1")
+    print("Hello 2")
+    print("Hello 3")
+
+print(name)
+
+# 只有在程序中，主动调用函数，才会让函数执行
+say_hello()
+
+print(name)
