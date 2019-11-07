fd0ce10555fcce30ac1fbb4353af360621f40b54
34431 <344319484@qq.com>
2019-11-07 21:14:57 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 风变编程/python基础-山腰班/fb_19.py
+++ 风变编程/python基础-山腰班/fb_19.py
@@ -2,3 +2,17 @@
 # 1.平日票价100元
 # 2.周末票价为平日票价120%
 # 3.儿童半价
+
+# 1.定义一个门票的类
+class Ticket:
+    # 2.定义
+    def __init__(self,adult,kid):
+        self.adult = adult
+        self.kid = kid
+
+    # 默认的票价为承认的票价
+#    def Price(self,adult_price,kid_price):
+ #       if
+
+
+
