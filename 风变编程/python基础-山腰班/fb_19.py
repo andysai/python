2fb9e1f8a39bd5360e12293ce74ad67deff65e1f
34431 <344319484@qq.com>
2019-11-08 14:40:20 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 风变编程/python基础-山腰班/fb_19.py
+++ 风变编程/python基础-山腰班/fb_19.py
@@ -6,13 +6,32 @@
 # 1.定义一个门票的类
 class Ticket:
     # 2.定义
-    def __init__(self,adult,kid):
-        self.adult = adult
-        self.kid = kid
+    def __init__(self,adult_number,kid_number):
+        self.adult_number = adult_number
+        self.kid_number = kid_number
 
     # 默认的票价为承认的票价
-#    def Price(self,adult_price,kid_price):
- #       if
+    def Price(self,adult_number,kid_number=1,price=100):
+        weekend = input("请问是否是周末(是/否):")
+        if weekend == '是':
+            one_adult_price = price * 1.2
+            all_adult_price = one_adult_price * adult_number  # 成人票价
+            one_kid_price = one_adult_price / 2
+            kid_price = one_kid_price * kid_number  # 儿童票价
+            ALL_price = all_adult_price + kid_price
+            message = '由于是周末,票价上涨20%,成年人票价为:{}元/张,成年人购票数量为:{}张,小孩票价为:{}元/张,小孩购票数量为:{}张,总价为:{}元'. \
+                format(one_adult_price, adult_number, one_kid_price, kid_number, ALL_price)
+            return message
+        elif weekend == '否':
+            adult_price = price * adult_number  # 成人票价
+            one_kid_price = price / 2
+            kid_price = one_kid_price * kid_number  # 儿童票价
+            ALL_price = adult_price + kid_price
+            message = '由于不是周末,票价不变,成年人票价为:{}元/张,成年人购票数量为:{}张,小孩票价为:{}元/张,小孩购票数量为:{}张,总价为:{}元'. \
+                format(price, adult_number, one_kid_price, kid_number, ALL_price)
+            return message
+        else:
+            return '请按照提示内容输入'
 
-
-
+manber = Ticket
+print(manber.Price(2,1))
