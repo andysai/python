24dc73cb59fcfd5286417a15c891a5bf18ce2fd2
34431 <344319484@qq.com>
2019-11-12 22:14:21 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 风变编程/python基础-山顶班/fb_01.py
@@ -0,0 +1,6 @@
+print('吴枫'.encode('utf-8'))
+print('吴枫'.encode('gbk'))
+print(b'\xe5\x90\xb4\xe6\x9e\xab'.decode('utf-8'))
+print(b'\xce\xe2\xb7\xe3'.decode('gbk'))
+
+print(b'\xe6\x88\x91\xe7\x88\xb1\xe4\xbd\xa0'.decode('utf-8'))
