bae1ba1b259852977f137e97b5be70c35f3d5475
34431 <344319484@qq.com>
2019-11-29 16:26:57 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 风变编程/python爬虫-山脚班/fb_05/fb_10.py
+++ 风变编程/python爬虫-山脚班/fb_05/fb_10.py
@@ -9,7 +9,7 @@ wl_company = input('请输入需要查询的物流公司:')
 # 输入需要查询的快递单号
 kuaidi_number = input('请输入需要查询的快递单号:')
 # 请输入浏览器上cookie值
-cookie = input('请输入网页的cookie值:')
+# cookie = input('请输入网页的cookie值:')
 
 # 伪装
 headers = {
@@ -17,7 +17,7 @@ headers = {
     'Referer': 'https://www.kuaidi100.com/',
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Connection': 'keep-alive',
-    'Cookie': cookie,
+    'Cookie': 'csrftoken=miQ-fFuQl81GqR4RGdGOJ3Wmp_W4txjYnFfJlAetolM; TOKEN=DewpDSy6Ubd_-W_poYAm3MzjEOXXmRHBWOr-lvo33K8; loginId=266566718; loginName=13686635733; nickname=13686635733; loginEmail=null; loginMobile=13686635733; loginExt=null; loginType=SELLER; sortStatus=0; auth=5; td_cookie=531570510; WWWID=WWW857947045EE4F341338D7D7B3B704251; loginSession=1; Hm_lvt_22ea01af58ba2be0fec7c11b25e88e6c=1575010586,1575015858; Hm_lpvt_22ea01af58ba2be0fec7c11b25e88e6c=1575015878',
     'Host': 'www.kuaidi100.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400',
     'X-Requested-With': 'XMLHttpRequest'
