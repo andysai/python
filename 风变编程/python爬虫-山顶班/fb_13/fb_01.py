0cb5bacb59ce648e06a3e203a752bada40b6a2c4
34431 <344319484@qq.com>
2019-12-10 23:33:43 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 风变编程/python爬虫-山顶班/fb_13/fb_01.py
+++ 风变编程/python爬虫-山顶班/fb_13/fb_01.py
@@ -1,1 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400'
+}
+
+for i in range(3):
+    url = 'https://book.douban.com/top250?start=' + str( i * 25 )
+    res = requests.get(url,headers=headers)
+    suops = BeautifulSoup(res.text,'html.parser').find_all('td',valign='top')
+    for suop in suops:
+        name = suop.find('a').text.strip()
+        message = suop.find('p')
+        print(name)
