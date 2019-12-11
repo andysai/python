2e8fabd6ee4cd92f0b283121016f38e77244f677
34431 <344319484@qq.com>
2019-12-11 20:53:36 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 风变编程/python爬虫-山顶班/fb_13/douban/douban/spiders/top250.py
@@ -0,0 +1,21 @@
+import scrapy,bs4
+from ..items import DoubanItem
+
+class DoubanSpider(scrapy.Spider):
+    name = 'douban'
+    allowed_domains = ['book.douban.com']
+    start_urls = []
+    for i in range(3):
+        url = 'https://book.douban.com/top250?start=' + str(i * 25)
+        start_urls.append(url)
+
+    def parse(self, response):
+        bs = bs4.BeautifulSoup(response.text,'html.parser')
+        datas = bs.find_all('div',class_='indent')
+        for data in datas:
+            item = DoubanItem()
+            item['title'] = data.find('a')[1]['title']
+            item['publish'] = data.find('p',class_='pl').text
+            item['score'] = data.find('span',class_='rating_nums').text
+            print(item['title'])
+            yield item
