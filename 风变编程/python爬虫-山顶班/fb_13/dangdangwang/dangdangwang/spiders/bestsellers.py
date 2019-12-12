626ae187ca156616bc7f259e15363c69e1ddfbc3
34431 <344319484@qq.com>
2019-12-12 17:09:41 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 风变编程/python爬虫-山顶班/fb_13/dangdangwang/dangdangwang/spiders/bestsellers.py
+++ 风变编程/python爬虫-山顶班/fb_13/dangdangwang/dangdangwang/spiders/bestsellers.py
@@ -1,4 +1,5 @@
-import scrapy,bs4
+import scrapy
+import bs4
 from ..items import DangdangwangItem
 
 class DangdangwangSpider(scrapy.Spider):
@@ -8,12 +9,16 @@ class DangdangwangSpider(scrapy.Spider):
     for i in range(3):
         url = 'http://bang.dangdang.com/books/bestsellers/01.00.00.00.00.00-year-2018-0-1-' + str(i + 1)
         url_lists.append(url)
+    print('1')
 
     def parse(self, response):
+        print('2')
         datas = bs4.BeautifulSoup(response.text,'html.parser').find('ul',class_='bang_list clearfix bang_list_mode').find_all('li')
         for data in datas:
             item = DangdangwangItem()
             item['name'] = data.find('div', class_="name").find('a')['title']
             item['author'] = data.find('div', class_="publisher_info").text
             item['price'] = data.find('div', class_="price").find('span', class_="price_n").text
+
             yield item
+        print('3')
