4a84cd64984c8193e962aa7fa32c585e8172f363
34431 <344319484@qq.com>
2019-12-13 22:44:07 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 风变编程/python爬虫-山顶班/fb_14/johui/johui/spiders/jobui_ jobs.py
@@ -0,0 +1,31 @@
+import scrapy
+import bs4
+from ..items import JohuiItem
+
+class JobuiSpider(scrapy.Spider):
+    name = 'jobs'
+    allowed_domains = ['www.jobui.com']
+    start_urls = ['https://www.jobui.com/rank/company/']
+
+    def parse(self, response):
+        bs = bs4.BeautifulSoup(response.text, 'html.parser')
+        ul_list = bs.find_all('ul',class_="textList flsty cfix")
+        for ul in ul_list:
+            a_list = ul.find_all('a')
+            for a in a_list:
+                company_id = a['href']
+                url = 'https://www.jobui.com{id}jobs'
+                real_url = url.format(id=company_id)
+                yield scrapy.Request(real_url, callback=self.parse_job)
+
+    def parse_job(self, response):
+        bs = bs4.BeautifulSoup(response.text, 'html.parser')
+        company = bs.find(id="companyH1").text
+        datas = bs.find_all('li', class_="company-job-list")
+        for data in datas:
+            item = JohuiItem()
+            item['company'] = company
+            item['position'] = data.find('h3').find('a').text
+            item['address'] = data.find('span', class_="col80").text
+            item['detail'] = data.find('span', class_="col150").text
+            yield item
