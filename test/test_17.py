4f6a0b01a31334d428c03172a07c0de1090d5241
34431 <344319484@qq.com>
2019-12-30 11:31:03 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- test/test_17.py
+++ test/test_17.py
@@ -5,27 +5,14 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400'
 }
 
-for i in range(3):
-    data = {
-        'op': 'search',
-        'type': 'm',
-        'pageindex': '8',
-        'salestatus': '',
-        'baoben': '',
-        'currency': '',
-        'term': '',
-        'keyword': '',
-        'series': '01',
-        'risk': '',
-        'city': '',
-        'date': '',
-        'pagesize': '20',
-        'orderby': 'ord1',
-        't': '0.9930362654056228',
-        'citycode': ''
-    }
-    url = 'http://www.cmbchina.com/cfweb/svrajax/product.ashx?op=search&type=m&pageindex=' + str(i) + '&salestatus=&baoben=&currency=&term=&keyword=&series=01&risk=&city=&date=&pagesize=20&orderby=ord1&t=0.9930362654056228&citycode='
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    suop = BeautifulSoup(res.text,'html.parser')
-    print(suop)
+url = 'https://www.993dy.com/vod-detail-id-13129.html'
+
+res_url = requests.get(url,headers=headers)
+print(res_url.status_code)
+res_url.encoding = 'utf-8'
+suop_urls = BeautifulSoup(res_url.text,'html.parser').find('ul',class_='downurl').find('script') # .find_all('div',class_='dwon_xl')
+print(suop_urls)
+#for i in suop_urls:
+#    link = i.find_all('div',class_='dwon_xl')
+    #print(link)
+
