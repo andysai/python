af651e4f9d5e282f4caf72d2b09d414587a8aee2
34431 <344319484@qq.com>
2020-02-29 02:39:29 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 实用代码/01_web展示excle数据.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_excle():
+    df = pd.read_excel("./故障统计表.xlsx")
+    table_html = df.to_html()
+    return f"""
+        <html>
+            <body>
+                <h1>故障统计表</h1>
+                <div>{table_html}</div>
+            </body>
+        </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host="192.168.31.184")
