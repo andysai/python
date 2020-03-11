6e01d4aede9c444bf25a3215e69a6a981dbee997
34431 <344349484@qq.com>
2020-03-11 22:31:34 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 风变-助教/山脚班/张婷-01.py
@@ -0,0 +1,17 @@
+a = 1
+while a < 101:
+    a = a + 1
+    print(a*5)
+
+a = 1
+while a < 101:
+    print(a*5)
+    a = a + 1
+
+
+man = ''
+#print(man)
+#while man != '有':
+#    man = input('aaa')
+#print('bbb')
+
