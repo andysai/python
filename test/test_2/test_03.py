7b47d49f19150971de3f503f56f25fb6e13f2a60
34431 <344349484@qq.com>
2020-03-21 22:43:49 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ test/test_2/test_03.py
@@ -0,0 +1,12 @@
+list = ['鸡腿','汉堡','烤串','炖肉']
+list.pop(2)
+print(list)
+list1 = ['鸡腿','汉堡','烤串','炖肉']
+list1.remove('烤串')
+print(list1)
+list2 = ['鸡腿','汉堡','烤串','炖肉']
+del list2[2]
+print(list2)
+list3 = ['鸡腿','汉堡','烤串','炖肉']
+list3.pop(2)
+print(list3)
---
+++ 风变-助教/山脚班-03-21/启启-01.py
@@ -0,0 +1,3 @@
+students = ['小明','小红','小刚']
+del students[0]
+print(students)
