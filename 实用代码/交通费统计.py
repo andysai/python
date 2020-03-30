73296364cc36127624908cae3a0eb4ad65bffec9
34431 <344349484@qq.com>
2020-03-30 21:07:35 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 实用代码/交通费统计.py
+++ 实用代码/交通费统计.py
@@ -1,13 +1,17 @@
-day_17 = 72.8+34
-day_18 = 73.4+39.5
-day_19 = 43.1+42.5
-day_20 = 34.48+40.5
-day_23 = 43.1+42.5
-day_24 = 43.1+42.5
-day_25 = 43.1+42.5
-day_26 = 43.1+42.5
-day_27 = 43.1+42.5
-day_30 = 43.1+42.5
+day_17 = 72.8 + 34
+day_18 = 73.4 + 39.5
+day_19 = 43.1 + 42.5
+day_20 = 34.48 + 40.5
+day_23 = 43.1 + 42.5
+day_24 = 43.1 + 42.5
+day_25 = 43.1 + 42.5
+day_26 = 43.1 + 42.5
+day_27 = 43.1 + 42.5
+day_30 = 43.1 + 42.5
+day_31 = 43.1 + 42.5
 
-all_fee = float(day_17) + float(day_18) + float(day_19) + float(day_20) + float(day_23) + float(day_24) + float(day_25)+ float(day_26) + float(day_27) + float(day_30)
+all_fee = round((float(day_17) + float(day_18) + float(day_19) + float(day_20) +
+                 float(day_23) + float(day_24) + float(day_25)+ float(day_26) + float(day_27)
+                 + float(day_30) + float(day_31)),2)
 print(all_fee)
+
