9d0e7b7136009cf4b2e6cfb166f928ddc92a781b
34431 <344349484@qq.com>
2020-05-27 21:14:02 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 黑马程序员-python/python从0开始学编程/day08/01-公共操作之运算符加号.py
@@ -0,0 +1,23 @@
+"""
+公共操作
+    运算符
+    公共方法
+    容器类型转换
+"""
+
+str1 = 'aa'
+str2 = 'bb'
+
+list1 = [1,2]
+list2 = [10,20]
+
+t1 = (1,2)
+t2 = (10,20)
+
+dict1 = {'name':'Python'}
+dict2 = {'age':30}
+
+# +:合并 字典不支持合并运算
+print(str1 + str2)
+print(list1 + list2)
+print(t1 + t2)
