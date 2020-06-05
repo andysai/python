bd765ee8df085c7ad4fb85dd951274ee0e1bde84
andysai <45644033+andysai@users.noreply.github.com>
2020-06-05 09:59:10 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ 黑马程序员-python/python从0开始学编程/day08/04-公共方法之len.py
@@ -0,0 +1,12 @@
+# len：计算容器中数据的个数
+str1 = 'abcdefg'
+list1 = [10,20,30,40,50]
+t1 = (10,20,30,40,50)
+s1 = {10,20,30,40,50}
+dict1 = {'name':'Tom','age':18}
+
+print(len(str1))
+print(len(list1))
+print(len(t1))
+print(len(s1))
+print(len(dict1))
