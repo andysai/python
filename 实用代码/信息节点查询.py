53e194068b1d682e88c0c9bc374bdb0dc32cbd40
34431 <344349484@qq.com>
2021-01-13 19:48:36 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

--- 实用代码/信息节点查询.py
+++ 实用代码/信息节点查询.py
@@ -1,5 +1,5 @@
 # 需求:通过信息节点或端口号判断网线在交换机的哪个端口
-# 1. 条件判断 if 或 for
+
 # 2.1 信息节点查询端口
 def Query_information():
     Information_point = int(input("请输入信息节点:"))
@@ -22,44 +22,43 @@ def Query_information():
         print('请输入正确的信息节点!')
 
 # 2.2 端口号查询信息点
-
-switch = int(input("请输入交换机编号:"))
-Port_number = int(input("请输入交换机端口号:"))
-if (Port_number >= 1 and Port_number <= 48) and switch == 1:
-    position = Port_number
-    print(f'信息点为{position}')
-elif (Port_number >= 1 and Port_number <= 48) and switch == 2:
-    position = Port_number + 49 - 1
-    print(f'信息点为{position}')
-elif (Port_number >= 1 and Port_number <= 48) and switch == 3:
-    position = Port_number + 144 - 1
-    print(f'信息点为{position}')
-elif (Port_number >= 1 and Port_number <= 48) and switch == 4:
-    position = Port_number + 192 - 1
-    print(f'信息点为{position}')
-elif (Port_number >= 1 and Port_number <= 48) and switch == 5:
-    position = Port_number + 193 - 1
-    print(f'信息点为{position}')
-else:
-    print("请输入正确的交换机编号和端口号")
-
-
-# 1. 定义参数
-def use_chooise():
-    chooise = input("请选择是通过信息点还是通过端口号查找信息(1 信息节点 or 2 端口号 ):")
-    if  chooise == '1':
-        pass
-    elif chooise == '2' :
-        pass
+def switch_port_message():
+    switch = int(input("请输入交换机编号:"))
+    Port_number = int(input("请输入交换机端口号:"))
+    if (Port_number >= 1 and Port_number <= 48) and switch == 1:
+        position = Port_number
+        print(f'信息点为{position}')
+    elif (Port_number >= 1 and Port_number <= 48) and switch == 2:
+        position = Port_number + 49 - 1
+        print(f'信息点为{position}')
+    elif (Port_number >= 1 and Port_number <= 48) and switch == 3:
+        position = Port_number + 144 - 1
+        print(f'信息点为{position}')
+    elif (Port_number >= 1 and Port_number <= 48) and switch == 4:
+        position = Port_number + 192 - 1
+        print(f'信息点为{position}')
+    elif (Port_number >= 1 and Port_number <= 48) and switch == 5:
+        position = Port_number + 193 - 1
+        print(f'信息点为{position}')
     else:
-        print("请按照提示进行选择!")
+        print("请输入正确的交换机编号和端口号")
 
 
-
+# 1. 定义用户须输入的参数
+def use_chooise():
+    a = True
+    while a:
+        chooise = input("请选择是通过信息点还是通过端口号查找信息(1 信息节点 or 2 端口号 or q 退出脚本 ):")
+        if chooise == '1':
+            Query_information()
+        elif chooise == '2' :
+            switch_port_message()
+        elif chooise == 'q':
+            print("退出脚本")
+            a = False
+        else:
+            print("请按照提示进行选择!")
 
 # 3. 调用函数
-if __name__ == '__mian__':
-    pass
-
-
-
+if __name__ == '__main__':
+    use_chooise()
