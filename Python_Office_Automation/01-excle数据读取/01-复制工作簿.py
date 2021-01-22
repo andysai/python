ebb9692da77a4ab6a76bf6a17ba1f7ce7868aed6
34431 <344349484@qq.com>
2021-01-22 16:01:15 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ Python_Office_Automation/01-excle数据读取/01-复制工作簿.py
@@ -0,0 +1,14 @@
+import xlrd
+import xlwt
+from xlutils.copy import copy
+
+# 首先进行行读取
+workbook = xlrd.open_workbook("source_material/2019年某公司员工薪资表.xls", formatting_info = True)
+sheet = workbook.sheets()[0]
+
+# 进行复制
+new_workbook = copy(workbook)
+new_sheet = new_workbook.get_sheet(0)
+
+# 保存
+new_workbook.save("source_material/1.xls")
