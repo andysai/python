ea16d1a475886847331bccfb8e43eac89d351465
34431 <344349484@qq.com>
2021-01-22 17:42:09 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ Python_Office_Automation/01-python自动办公基础/01-excle数据读取/01-第三方库的安装.py
@@ -0,0 +1,11 @@
+"""
+第三方库:xlrd xlwt
+python的标准库:是随着python安装的时候默认自带的库
+python的第三方库:是第三方公司提供的，需要下载安装(简单)
+
+读取excle数据的库
+    库名:xlrd -- xlrd其实是excle read的简写形式
+
+写入excle数据的库
+    库名:xlwt -- xlwt其实是exlce write的简写形式
+"""
