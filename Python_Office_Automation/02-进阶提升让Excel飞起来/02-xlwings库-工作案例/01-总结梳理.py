2fb65c976560b73ee7d845be86833f71636b919b
sai <344319484@qq.com>
2021-01-28 16:35:41 +0800

    Merge branch 'master' of https://github.com/andysai/python
    # Conflicts:
    #	.idea/workspace.xml

---
+++ Python_Office_Automation/02-进阶提升让Excel飞起来/02-xlwings库-工作案例/01-总结梳理.py
@@ -0,0 +1,7 @@
+"""
+xlwings: import xlwings as xw
+APP: app = xw.APP(visible=True,add_book=False)
+Book:打开原工作簿:workbook=app.books.open('文件路径') 打开新工作簿(未讲):workbook=app.books.add()
+sheets:
+range:
+"""
